08fe1695bc4ed873270dd8ca1bfc0e65aea0ddf0
Alexandros Papakostopoulos <a.papakostopoulos@gmail.com>
2021-10-13 21:25:48 +0300

    env

--- scalper/settings.py
+++ scalper/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import environ
 from pathlib import Path
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ity@@=w=fa644@vc=n&e)zu-p=#8(yy!p9sbkjlil&8%9txy^*'
+#SECRET_KEY = 'ity@@=w=fa644@vc=n&e)zu-p=#8(yy!p9sbkjlil&8%9txy^*'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
