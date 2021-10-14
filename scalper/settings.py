d1449c77bb66452065704d73bc306caadef44d4d
Alexandros Papakostopoulos <a.papakostopoulos@gmail.com>
2021-10-14 21:47:16 +0300

    settings

--- scalper/settings.py
+++ scalper/settings.py
@@ -74,7 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'option_pricing.context_processors.future_bar',
+                
             ],
         },
     },
