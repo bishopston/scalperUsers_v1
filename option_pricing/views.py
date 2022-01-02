4f9c005f69ce96c66b6991482dd66d50c8ea820e
Alexandros Papakostopoulos <a.papakostopoulos@gmail.com>
2022-01-02 20:12:23 +0200

    dummy2

--- option_pricing/views.py
+++ option_pricing/views.py
@@ -3,7 +3,7 @@ from django.db.models import Max, Min, Avg, Sum, F
 from django.core.paginator import Paginator
 from django.http import JsonResponse, HttpResponse, FileResponse, Http404
 from django.views.generic import View
-from django.template import loader
+#from django.template import loader
 from django.contrib.auth.decorators import login_required
 from django.views.generic.base import TemplateView
 #from django.core import serializers
